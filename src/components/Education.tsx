60ac61d8d8e835d8548025e3d09dcfaceece0ac7
sreyas <sreyas1045@gmail.com>
2024-12-02 19:03:49 +0530

    made some changes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,15 +1,18 @@
-
 function Education() {
-
   return (
     <>
-      <main  className="flex flex-col items-start gap-8 w-full  flex-wrap  text-text py-5">
+      <main className="flex flex-col items-start gap-8 w-full  flex-wrap  text-text py-5">
         <div className="text-center font-semibold text-[18px] w-full text-accent flex items-center justify-center py-5">
-          <p className="px-3 py-1 border max-w-[120px] text-center rounded-lg">Education</p>
+          <p className="px-3 py-1 border max-w-[120px] text-center rounded-lg">
+            Education
+          </p>
         </div>
-        <div className="flex justify-start   gap-6 items-start p-5  py-8 border w-full rounded-lg">{/*card*/}
+        <div className="flex justify-start   gap-6 items-start p-5  py-8 border w-full rounded-lg">
+          {/*card*/}
           <div className="bg-black text-white   h-full flex items-center justify-center ">
-            <p className="w-[60px] h-[60px] rounded-[50%] flex items-center justify-center">MEC</p>
+            <p className="w-[60px] h-[60px] rounded-[50%] flex items-center justify-center">
+              MEC
+            </p>
           </div>
           <main className="flex flex-col text-[15px] gap-1">
             <header>Govt. Model Engineering College</header>
